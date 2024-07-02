88db0dc748d6487fdb5d93010c5996e4e0c3ee47
Sreegopan K R <sreegopankr@gmail.com>
2024-07-02 19:48:26 +0530

    sameSite made none

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req,res,next) =>{
     const token = req.cookies.token;
-
+    
     if(!token) return res.status(401).json({message: "Not Authenticated!"});
 
     jwt.verify(token,process.env.JWT_SECRET,(err,payload)=>{
